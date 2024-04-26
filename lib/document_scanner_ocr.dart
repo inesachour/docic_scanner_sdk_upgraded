3fa27fa0720065816f1448735272855a42f1c7d6
inesachour <ines.achour@insat.ucar.tn>
2024-04-26 10:44:25 +0100

    add screens folder

--- lib/document_scanner_ocr.dart
+++ lib/document_scanner_ocr.dart
@@ -1,4 +1,4 @@
-import 'package:document_scanner_ocr/src/widgets/camera_screen.dart';
+import 'package:document_scanner_ocr/src/widgets/screens/camera_screen.dart';
 import 'package:flutter/material.dart';
 
 class DocumentScannerOcr extends StatefulWidget {
